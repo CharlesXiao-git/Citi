b5531919a7e7b1a18772aa6a5fee4be34ec1d9e5
bixiao <xiaobi808@yahoo.com>
2019-10-11 18:09:20 +0800

    src

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,73 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'JOB CREATION',
+    icon: 'shopping-cart-outline',
+    link: '/pages/tables/smart-table',
+    home: true,
+  },
+  {
+    title: 'JOB LIST',
+    icon: 'home-outline',
+    link: '/pages/tables/tree-grid',
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
+    title: 'Forms',
+    icon: 'edit-2-outline',
+    children: [
+      {
+        title: 'Form Inputs',
+        link: '/pages/forms/inputs',
+      },
+      {
+        title: 'Form Layouts',
+        link: '/pages/forms/layouts',
+      },
+      {
+        title: 'Buttons',
+        link: '/pages/forms/buttons',
+      },
+    ],
+  },
+  {
+    title: 'Tables & Data',
+    icon: 'grid-outline',
+    children: [
+      {
+        title: 'Smart Table',
+        link: '/pages/tables/smart-table',
+      },
+      {
+        title: 'Tree Grid',
+        link: '/pages/tables/tree-grid',
+      },
+    ],
+  },
+  {
+    title: 'Auth',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
+];
